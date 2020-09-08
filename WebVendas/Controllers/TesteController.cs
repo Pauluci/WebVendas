eb0f2a23d799e468cfb8da4a913b2cb2627af080
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2020-09-08 18:31:45 -0300

    Adicionar arquivos de projeto.

---
+++ WebVendas/Controllers/TesteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVendas.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Mensagem"] = "Esta é Página de teste";
+            ViewData["nome"] = "Luan Honorio"; 
+            ViewData["Email"] = "honorioluan@gmail.com";
+            ViewData["numero"] = "2019073840";
+            ViewData["meteria"] = "orientação a objeto";
+            ViewData["dias"] = "Terça-Feira";
+            ViewData["bim"] = "2 Bimestre";
+            return View();
+        }
+    }
+}
---
+++ WebVendas/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace WebVendas.Models
+{
+    public class Department
+    {
+        public  int id { get; set; }
+        public string Name { get; set;}
+    }
+}
